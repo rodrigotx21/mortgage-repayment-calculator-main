58653b8afed60028c05560f167c55578402cd8b4
Rodrigo <rodrigotteixeira7@gmail.com>
2024-07-06 23:52:11 +0100

    responsive

--- src/script.js
+++ src/script.js
@@ -3,14 +3,19 @@ let type = null;
 function select(selected) {
     let repayment = document.getElementById("repayment");
     let interest = document.getElementById("interest");
+    let inputs = document.getElementsByClassName("radio_input");
     if (selected == "repayment") {
         type = "repayment";
         repayment.checked = true;
         interest.checked = false;
+        inputs[0].classList.add("radio_input_active");
+        inputs[1].classList.remove("radio_input_active");
     } else if (selected == "interest") {
         type = "interest";
         repayment.checked = false;
         interest.checked = true;
+        inputs[1].classList.add("radio_input_active");
+        inputs[0].classList.remove("radio_input_active");
     }
 }
 
@@ -38,3 +43,17 @@ function calculate(event) {
     document.getElementById("total").innerHTML = (repayment * n).toFixed(2);
     switch_right();
 }
+
+
+/* Input Focus */
+[["amount", 0], ["term", 1], ["rate", 2]].forEach(([input, index]) => {
+    document.getElementById(input).addEventListener("focus", function() {
+        document.getElementsByClassName("input")[index].classList.add("input_focus");
+        document.getElementsByClassName("icon_text")[index].classList.add("icon_text_focus");
+    });
+    
+    document.getElementById(input).addEventListener("blur", function() {
+        document.getElementsByClassName("input")[index].classList.remove("input_focus");
+        document.getElementsByClassName("icon_text")[index].classList.remove("icon_text_focus");
+    });
+});
